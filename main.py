411d52a43e6e2fd81c5f773583149928e4c87363
dalton-0x0 <12995327+dalton-0x0@users.noreply.github.com>
2023-12-25 07:40:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Write your code below this line 👇
+
+# new line \n
+print("hello world!\nhello world!!\n")
+
+# concatenate strings
+print("hello" + " howard\n")
+
+# input function
+print("hello " + input("what is your name? "))
+
+# quit program
+# quit()
